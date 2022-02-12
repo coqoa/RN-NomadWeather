d7856a39193ed7cf875f42edf3c01eaecd20c829
coqoa <choibm0208@gmail.com>
2022-02-12 17:16:28 +0900

    220212 css작업

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,27 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function App() {
   return (
-    <View style={{flex: 1, flexDirection: "row"}}>
-      <View style={{flex: 1, flexDirection: "row"}}>
-        <View style={{flex: 1, backgroundColor: "tomato"}}></View>
-        <View style={{flex: 1, backgroundColor: "teal"}}></View>
-        <View style={{flex: 1, backgroundColor: "orange"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <Text>-</Text>
-      <View style={{flex: 2}}>
-        <View style={{flex: 1, backgroundColor: "tomato"}}></View>
-        <View style={{flex: 1, backgroundColor: "teal"}}></View>
-        <View style={{flex: 1, backgroundColor: "orange"}}></View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desciption}>Sunny</Text>
+        </View>
       </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    backgroundColor: "salmon"
+  },
+  city:{
+    flex: 1,
+    backgroundColor: "tomato",
+    justifyContent: "center",
+    alignItems:"center",
+  },
+  cityName:{
+    color:"black",
+    fontSize:50,
+    fontWeight: "500"
+  },
+  weather:{
+    flex: 3,
+  },
+  day:{
+    flex: 1,
+    alignItems:"center",
+    // backgroundColor: "teal",
+  },
+  temp:{
+    marginTop: 50,
+    fontSize: 160,
+  },
+  desciption:{
+    marginTop: -30,
+    fontSize: 60,
   },
 });
